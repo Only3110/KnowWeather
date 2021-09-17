a48f75b6cf181ce24b7dd55717e32b8f75b02c0a
Only3110 <86710510+Only3110@users.noreply.github.com>
2021-09-17 08:18:53 +0000

    edt2

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
+const addBtn = document.querySelector('.addbtn');
+var input = document.querySelector('.txt');
+const container = document.querySelector('.container');
 
+class  item{
+      constructor(itemName){
+            this.createDiv(itemName);
+      }
+      createDiv(itemName){
+            let input = document.createElement('input');
+            input.value = itemName;
+            input.disabled = true;
+            input.classList.add('item_input');
+            input.type = "text";
+
+            let itemBox = document.createElement('div');
+            itemBox.classList.add('item');
+
+            let editBtn = document.createElement('button');
+            editBtn.innerHTML = "<i class='bx bx-pencil'></i>";
+            editBtn.classList.add('editbtn');
+            
+            let rmvBtn = document.createElement('button');
+            rmvBtn.innerHTML = "<i class='bx bx-trash-alt'></i>";
+            rmvBtn.classList.add('rmvbtn');
+
+            container.appendChild(itemBox);
+
+            itemBox.appendChild(input);
+            itemBox.appendChild(editBtn);
+            itemBox.appendChild(rmvBtn);
+
+            editBtn.addEventListener('click', ( ) => this.edit(input) );
+            rmvBtn.addEventListener('click', ( ) => this.remove(itemBox) );
+            
+      }
+      edit(input){
+            input.disabled = !input.disabled;
+      }
+      remove(item){
+            container.removeChild(item);
+      }
+}
+
+function check(){
+      if(input.value != ""){
+            new item(input.value);
+            input.value = "";
+      }
+}
+
+addBtn.addEventListener('click', check);
